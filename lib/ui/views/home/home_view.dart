1101676d60f8f406d0a2d11c1fb807a417671003
namtien151 <tntien151@gmail.com>
2024-08-20 16:25:42 +0700

    abccc

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -1,10 +1,9 @@
+import 'dart:io';
 import 'package:file_picker/file_picker.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-
 import 'package:stacked/stacked.dart';
 import 'package:dotted_border/dotted_border.dart';
+import 'package:path/path.dart' as path;
 
 import 'home_viewmodel.dart';
 
@@ -29,17 +28,17 @@ class HomeView extends StackedView<HomeViewModel> {
               boxShadow: const [
                 BoxShadow(
                   color: Colors.black26,
-                  offset: Offset(0, 4), // Vị trí bóng
-                  blurRadius: 10, // Độ mờ của bóng
-                  spreadRadius: 1, // Độ lan rộng của bóng
+                  offset: Offset(0, 4),
+                  blurRadius: 10,
+                  spreadRadius: 1,
                 ),
               ],
             ),
             child: Padding(
-              padding: EdgeInsets.all(20.0), // Khoảng cách với đường viền
+              padding: EdgeInsets.all(10.0),
               child: Column(
                 children: [
-                  SizedBox(height: 20), // Khoảng cách giữa các phần tử
+                  SizedBox(height: 20),
                   DottedBorder(
                     color: Colors.black26,
                     strokeWidth: 1.5,
@@ -75,9 +74,7 @@ class HomeView extends StackedView<HomeViewModel> {
                                 fontWeight: FontWeight.w400,
                                 color: Colors.black26),
                           ),
-                          SizedBox(
-                            height: 10,
-                          ),
+                          SizedBox(height: 10),
                           ElevatedButton(
                             style: ElevatedButton.styleFrom(
                               foregroundColor: Colors.black26,
@@ -92,16 +89,7 @@ class HomeView extends StackedView<HomeViewModel> {
                                   horizontal: 30, vertical: 15),
                             ),
                             onPressed: () async {
-                              FilePickerResult? result = await FilePicker
-                                  .platform
-                                  .pickFiles(allowMultiple: true);
-
-                              if (result != null) {
-                                List<PlatformFile> files = result.files;
-                                viewModel.updateSelectedFiles(files);
-                              } else {
-                                // User canceled the picker
-                              }
+                              await viewModel.pickFolder();
                             },
                             child: Text(
                               'Choose Files',
@@ -112,102 +100,163 @@ class HomeView extends StackedView<HomeViewModel> {
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  SingleChildScrollView(
-                    child: Container(
-                      width: 900,
-                      height: 400,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(15),
-                        border: Border.all(
-                          color: Colors.black12,
-                          width: 1,
-                        ),
-                      ),
-                      child: viewModel.selectedFiles != null &&
-                          viewModel.selectedFiles!.isNotEmpty
-                          ? ListView.builder(
-                        itemCount: viewModel.selectedFiles!.length,
-                        itemBuilder: (context, index) {
-                          PlatformFile fileItem =
-                          viewModel.selectedFiles![index];
-                          FileStatusType status =
-                              viewModel.fileStatuses[index].status;
-                          return Container(
-                            margin: EdgeInsets.symmetric(
-                                vertical: 5, horizontal: 10),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(9),
-                              boxShadow: const [
-                                BoxShadow(
-                                  color: Colors.black26,
-                                  offset: Offset(0, 3), // Vị trí bóng
-                                  blurRadius: 10, // Độ mờ của bóng
-                                  spreadRadius:
-                                  0.5, // Độ lan rộng của bóng
-                                ),
-                              ],
-                            ),
-                            child: ListTile(
-                              title: Text(
-                                fileItem.name,
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w500),
-                              ),
-                              subtitle: Text(
-                                fileItem.size < 1024 * 1024
-                                    ? '${(fileItem.size / 1024).toStringAsFixed(2)} KB' // Nếu kích thước nhỏ hơn 1 MB, hiển thị theo KB
-                                    : '${(fileItem.size / 1024 / 1024).toStringAsFixed(2)} MB', // Nếu kích thước lớn hơn 1 MB, hiển thị theo MB
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w100,
-                                ),
-                              ),
-
-
-                              trailing: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  // Icon trạng thái dựa trên điều kiện
-                                  status == FileStatusType.idle
-                                      ? Image.asset(
-                                      'assets/image/close.png')
-                                      : status == FileStatusType.loading
-                                      ? CircularProgressIndicator() // Biểu tượng loading
-                                      : Icon(Icons.check_circle,
-                                      color: Colors
-                                          .green), // Trạng thái done
-
-                                  // Icon delete luôn hiển thị
-                                  IconButton(
-                                    icon: Icon(Icons.delete,
-                                        color: Colors.redAccent),
-                                    onPressed: () {
-                                      viewModel.removeFile(index);
-                                    },
+                  SizedBox(height: 10),
+                  viewModel.selectedDirectory != null
+                      ? Column(
+                          children: [
+                            Container(
+                              width: 900,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(12),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.2), // Shadow color
+                                    offset: Offset(0, 4), // Shadow offset
+                                    blurRadius: 8, // Shadow blur radius
+                                    spreadRadius: 1, // Shadow spread radius
                                   ),
                                 ],
                               ),
+                              child: ListTile(
+                                leading: Icon(Icons.folder, color: Colors.amber),
+                                title: Text(
+                                  '${viewModel.selectedDirectory!}',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                trailing: IconButton(
+                                  icon: Icon(viewModel.showFiles
+                                      ? Icons.arrow_drop_up
+                                      : Icons.arrow_drop_down),
+                                  onPressed: () {
+                                    viewModel.toggleFilesVisibility();
+                                  },
+                                ),
+                              ),
                             ),
-                          );
-                        },
-                      )
-                          : Center(
-                        child: Text(
-                          'No files selected',
-                          style: TextStyle(color: Colors.grey),
+
+                            if (viewModel.showFiles)
+                              Padding(
+                                padding: const EdgeInsets.only(top: 10),
+                                child: Container(
+                                  width: 900,
+                                  height: 370,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(15),
+                                    border: Border.all(
+                                      color: Colors.black12,
+                                      width: 1,
+                                    ),
+
+                                  ),
+                                  child: viewModel.files != null &&
+                                          viewModel.files!.isNotEmpty
+                                      ? ListView.builder(
+                                          itemCount: viewModel.files!.length,
+                                          itemBuilder: (context, index) {
+                                            FileSystemEntity file =
+                                                viewModel.files![index];
+                                            FileStatusType status = viewModel
+                                                .fileStatuses[index].status;
+                                            return Container(
+                                              margin: EdgeInsets.symmetric(
+                                                  vertical: 5, horizontal: 10),
+                                              decoration: BoxDecoration(
+                                                color: Colors.white,
+                                                borderRadius:
+                                                    BorderRadius.circular(9),
+                                                boxShadow: const [
+                                                  BoxShadow(
+                                                    color: Colors.black26,
+                                                    offset: Offset(0, 3),
+                                                    blurRadius: 10,
+                                                    spreadRadius: 0.5,
+                                                  ),
+                                                ],
+                                              ),
+                                              child: ListTile(
+                                                title: Text(
+                                                  path.basename(file.path),
+                                                  style: TextStyle(
+                                                    fontSize: 16,
+                                                    fontWeight: FontWeight.w500,
+                                                  ),
+                                                ),
+                                                subtitle: Column(
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.start,
+                                                  children: [
+                                                    // Hiển thị kích thước tệp nếu là tệp
+                                                    file is File
+                                                        ? Text(
+                                                            '${viewModel.formatFileSize(File(file.path).lengthSync())}', // Hiển thị kích thước tệp
+                                                            style: TextStyle(
+                                                              fontSize: 14,
+                                                              fontWeight:
+                                                                  FontWeight.w300,
+                                                            ),
+                                                          )
+                                                        : Container(),
+                                                    // Hiển thị đường dẫn tệp
+                                                    Text(
+                                                      file.path, // Hiển thị đường dẫn đầy đủ
+                                                      style: TextStyle(
+                                                        fontSize: 12,
+                                                        color: Colors.grey,
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                                trailing: Row(
+                                                  mainAxisSize: MainAxisSize.min,
+                                                  children: [
+                                                    status == FileStatusType.idle
+                                                        ? Image.asset(
+                                                            'assets/image/close.png')
+                                                        : status ==
+                                                                FileStatusType
+                                                                    .loading
+                                                            ? CircularProgressIndicator()
+                                                            : Icon(
+                                                                Icons
+                                                                    .check_circle,
+                                                                color:
+                                                                    Colors.green,
+                                                              ),
+                                                    IconButton(
+                                                      icon: Icon(Icons.delete,
+                                                          color:
+                                                              Colors.redAccent),
+                                                      onPressed: () {
+                                                        viewModel
+                                                            .removeFile(index);
+                                                      },
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            );
+                                          },
+                                        )
+                                      : Center(
+                                          child: Text(
+                                            'No files selected',
+                                            style: TextStyle(color: Colors.grey),
+                                          ),
+                                        ),
+                                ),
+                              ),
+                          ],
+                        )
+                      : Center(
+                          child: Text(
+                            'No folder selected',
+                            style: TextStyle(color: Colors.grey),
+                          ),
                         ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
+                  SizedBox(height: 15),
                   ClipRRect(
                     borderRadius: BorderRadius.circular(15),
                     child: Container(
@@ -220,18 +269,15 @@ class HomeView extends StackedView<HomeViewModel> {
                           width: 1,
                         ),
                       ),
+
                       child: LinearProgressIndicator(
                         value: viewModel.progressValue,
                         backgroundColor: Colors.grey,
-                        valueColor: AlwaysStoppedAnimation<Color>(
-                            Colors.green),
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
                       ),
                     ),
                   ),
-
-                  SizedBox(
-                    height: 10,
-                  ),
+                  SizedBox(height: 8),
                   ElevatedButton(
                     onPressed: () async {
                       await viewModel.rotateFiles();
@@ -239,9 +285,8 @@ class HomeView extends StackedView<HomeViewModel> {
                     child: Row(
                       mainAxisSize: MainAxisSize.min,
                       children: [
-                        Icon(Icons.rotate_right,
-                            color: Colors.black26), // Thay đổi icon theo ý muốn
-                        SizedBox(width: 8), // Khoảng cách giữa icon và văn bản
+                        Icon(Icons.rotate_right, color: Colors.black26),
+                        SizedBox(width: 8),
                         Text(
                           'Xoay',
                           style: TextStyle(color: Colors.black),
@@ -258,7 +303,7 @@ class HomeView extends StackedView<HomeViewModel> {
                         side: BorderSide(color: Colors.black26),
                       ),
                       padding:
-                      EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                          EdgeInsets.symmetric(horizontal: 50, vertical: 20),
                     ),
                   )
                 ],
--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -1,52 +1,34 @@
+import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:stacked/stacked.dart';
-import 'package:roatate_file_tool/app/app.locator.dart';
-import 'package:roatate_file_tool/app/app.router.dart';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:path/path.dart' as path;
 import 'package:stacked_services/stacked_services.dart';
+import 'dart:math';
+import '../../../app/app.locator.dart';
 
 class HomeViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
-  List<PlatformFile>? selectedFiles = [];
+
   List<FileStatus> fileStatuses = [];
   double progressValue = 0.0;
-  StartupViewModel() {
-    // Initialize fileStatuses based on the number of selected files.
-    fileStatuses = List.generate(selectedFiles?.length ?? 0, (index) => FileStatus(id: index, status: FileStatusType.idle));
-  }
+  bool _showFiles = false;
+  String? _selectedDirectory;
+  List<FileSystemEntity>? _files;
+  bool get showFiles => _showFiles;
+
+  String? get selectedDirectory => _selectedDirectory;
+  List<FileSystemEntity>? get files => _files;
 
   void removeFile(int index) {
-    selectedFiles?.removeAt(index);
+    files?.removeAt(index);
     notifyListeners();
   }
-  void updateSelectedFiles(List<PlatformFile> files) {
-    // Nếu selectedFiles chưa được khởi tạo, khởi tạo nó
-    selectedFiles ??= [];
-
-    // Lưu lại số lượng file hiện tại
-    int previousLength = selectedFiles!.length;
-
-    // Thêm tất cả các file mới vào danh sách hiện có
-    selectedFiles!.addAll(files);
-
-    // Cập nhật trạng thái cho các file đã có và file mới
-    fileStatuses = List.generate(
-      selectedFiles!.length,
-          (index) => FileStatus(
-          id: index,
-          status: index < previousLength ? fileStatuses[index].status : FileStatusType.idle
-      ),
-    );
-
-    // Thông báo cập nhật giao diện
-    notifyListeners();
-  }
-
-
 
   Future<void> rotateFiles() async {
-    if (selectedFiles == null || selectedFiles!.isEmpty) return;
+    if (files == null || files!.isEmpty) return;
 
-    int totalFiles = selectedFiles!.length;
+    int totalFiles = files!.length;
     progressValue = 0.0; // Reset progress
 
     for (int i = 0; i < totalFiles; i++) {
@@ -55,14 +37,64 @@ class HomeViewModel extends BaseViewModel {
     notifyListeners();
 
     for (int i = 0; i < totalFiles; i++) {
-      await Future.delayed(Duration(seconds: 2)); // Simulate delay
+      await Future.delayed(Duration(seconds: 1)); // Simulate delay
       fileStatuses[i].status = FileStatusType.done;
       progressValue = (i + 1) / totalFiles; // Update progress
       notifyListeners();
     }
   }
 
+  Future<void> pickFolder() async {
+    String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
+
+    if (selectedDirectory != null) {
+      _selectedDirectory = selectedDirectory;
+
+      // Lấy tất cả các tệp PDF và hình ảnh trong thư mục và các thư mục con
+      _files = _getFilesFromDirectory(Directory(selectedDirectory));
+      fileStatuses = List.generate(_files!.length,
+              (index) => FileStatus(id: index, status: FileStatusType.idle));
+      _showFiles = false; // Ẩn danh sách file
+      notifyListeners();
+    }
+  }
+
+  List<FileSystemEntity> _getFilesFromDirectory(Directory directory) {
+    List<FileSystemEntity> files = [];
+    try {
+      directory.listSync(recursive: true).forEach((file) {
+        if (file is File && _isValidFileType(file)) {
+          files.add(file);
+        }
+      });
+    } catch (e) {
+      print('Error reading directory: $e');
+    }
+    return files;
+  }
+
+  bool _isValidFileType(File file) {
+    final extension = file.uri.pathSegments.last.split('.').last.toLowerCase();
+    return extension == 'pdf' || _isImageExtension(extension);
+  }
+
+  bool _isImageExtension(String extension) {
+    const imageExtensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff'];
+    return imageExtensions.contains(extension);
+  }
+
+  String formatFileSize(int bytes) {
+    if (bytes <= 0) return '0 B';
+    const units = ['B', 'KB', 'MB', 'GB', 'TB'];
+    final i = (log(bytes) / log(1024)).floor();
+    return '${(bytes / pow(1024, i)).toStringAsFixed(2)} ${units[i]}';
+  }
+  void toggleFilesVisibility() {
+    _showFiles = !_showFiles;
+    notifyListeners();
+  }
 }
+
 class FileStatus {
   final int id;
   FileStatusType status;
